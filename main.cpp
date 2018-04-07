dc9fc150c83f09db7ee8269437b9ef0156de12f2
zzw.0628 <zhuzhenwei789@gmail.com>
2018-04-07 22:06:50 +0800

    add a new file

--- main.cpp
+++ main.cpp
@@ -4,98 +4,120 @@
 #define PI 3.1415926
 using namespace std;
 int main()
-{ 
-    cout << "begining" << endl;
-	// 6¸ö¾§Á££¬Íø¸ñÎª200 * 200 
-	const int N = 6; 
-	const int Nx = 100;
-	const int Ny = 100;
-	double phi[N][Nx][Ny]; // µ±Ç°Ê±¿ÌµÄphi
-	double phi_b[N][Nx][Ny]; // ÉÏÒ»Ê±¿ÌµÄphi_b 
-	
-	// ³õÊ¼»¯Ã¿¸ö¾§Á£ÔÚ²»Í¬Íø¸ñ´¦µÄphiÖµ 
-	int n, i, j;
-	for (n = 0; n < N; n++)
-	{
-		for (i = 0; i < Nx; i++)
-		{
-			for (j = 0; j < Ny; j++)
-			{ 
-				phi[n][i][j] = phi_b[n][i][j] = 0; // ½«¾§Á£ÔÚËùÓÐÍø¸ñµÄphi¶¼ÉèÖÃÎª0£»
-				
-				// ½«6¸ö¾§Á£½øÐÐ³õÊ¼»¯£¬¼´ÌØ¶¨Î»ÖÃµÄphiÎª1£¬ÆäËûÎ»ÖÃ¶¼ÊÇ0
-				// ÖÐ¼äµÄÈý¸ö½øÐÐ³õÊ¼»¯ 
-				if (n == 0 || n == 1 || n == 2) {
-					if (j <= Ny/2 + 3 && j >= Ny/2 - 3){
-					    if (n == 0 && (i <= Nx/6 + 3 && i >= Nx/6 - 3)) {
-					    	phi[0][i][j] = phi_b[0][i][j] = 1; // µÚÒ»¸ö 
-					    }
-					    else if (n == 1 && (i <= (Nx/6)*3 + 3 && i >= (Nx/6)*3 - 3 )) {
-					    	phi[1][i][j] = phi_b[1][i][j] = 1; // µÚ¶þ¸ö 
-					    }
-					    else if (n == 2 && (i <= (Nx/6)*5 + 3 && i >= (Nx/6)*5 - 3 )) {
-					    	phi[2][i][j] = phi[2][i][j] = 1; // µÚÈý¸ö 
-				    	}
-					} 
-			    } 
-				else if (n == 3 || n == 4) {
-					// ÉÏÏÂµÄÁ½¸ö¾§Á£³õÊ¼»¯
-				    if (j <= 3 || j >= Ny - 3) {
-						if (n == 3 && (i <= Nx/3 + 3 && i >= Nx/3 - 3)) {
-							phi[3][i][j] = phi_b[3][i][j] = 1; // µÚËÄ¸ö 
-						}
-						else if (n == 4 && (i <= (Nx/3)*2 + 3 && i >= (Nx/3)*2 - 3)) {
-							phi[4][i][j] = phi_b[4][i][j] = 1; // µÚÎå¸ö 
-						}
-					} 
-				}
-				// ËÄ¸ö½ÇµÄ¾§Á£
-				else if (n == 5){
-					if ((i <= 3 && j <= 3) || (i >= Nx - 3 && j <= 3) || (i >= Nx - 3 && j >= Ny - 3) || (i <= 3 && j >= Ny - 3)) {
-						phi[5][i][j] = phi_b[5][i][j] = 1; // µÚÁù¸ö 
-					}
-				}
-			}
-		}
-	}
-	
-	
+{
+  cout << "begining" << endl;
+  // 6个晶粒，网格为100 * 100 
+  const int N = 6;
+  const int Nx = 100;
+  const int Ny = 100;
+  const int deltaX = 1;
+  double phi[N][Nx][Ny]; // 当前时刻的phi
+  double phi_b[N][Nx][Ny]; // 下一时刻的phi_b 
 
-// ²âÊÔ³õÊ¼»¯Ê¹ÓÃ  
-// Í¨¹ýÒÔÏÂ³ÌÐò¿ÉÒÔ²âÊÔ³öÔÚ³õÊ¼»¯×´Ì¬ÏÂÃ¿¸ö¾§Á£µÄphi²»Îª0µÄÎ»ÖÃ¡£ 
-		for (int i = 0; i < Nx; i++)
-		{
-			for (int j = 0; j < Ny; j++)
-			{
-				if (phi[2][i][j] == 1) {
-					cout << i << '\t' << j << endl;
-				}
-			}
-		}
+  // 初始化每个晶粒在不同网格处的phi值 
+  int n, i, j;
+  for (n = 0; n < N; n++)
+  {
+    for (i = 0; i < Nx; i++)
+    {
+      for (j = 0; j < Ny; j++)
+      {
+        phi[n][i][j] = phi_b[n][i][j] = 0; // 将晶粒在所有网格的phi都设置为0；
 
-//  cout << phi[3][62][198] << phi[3][70][1] << phi[3][67][196] << phi[3][68][2] << endl; // ²âÊÔ³õÊ¼»¯Ê¹ÓÃ 
-//	cout << "ÉÏÒ»¸öÊ±¿ÌµÚÁù¸ö¾§Á£ÔÚÔ­µã´¦µÄphiÖµ" << phi_b[5][0][0] << endl; // ²âÊÔphi_bµÄ³õÊ¼»¯Ê¹ÓÃ 
+        // 将6个晶粒进行初始化，即特定位置的phi为1，其他位置都是0
+        // 中间的三个进行初始化 
+        if (n == 0 || n == 1 || n == 2) {
+          if (j <= Ny / 2 + 3 && j >= Ny / 2 - 3){
+            if (n == 0 && (i <= Nx / 6 + 3 && i >= Nx / 6 - 3)) {
+              phi[0][i][j] = phi_b[0][i][j] = 1; // 第一个 
+            }
+            else if (n == 1 && (i <= (Nx / 6) * 3 + 3 && i >= (Nx / 6) * 3 - 3)) {
+              phi[1][i][j] = phi_b[1][i][j] = 1; // 第二个 
+            }
+            else if (n == 2 && (i <= (Nx / 6) * 5 + 3 && i >= (Nx / 6) * 5 - 3)) {
+              phi[2][i][j] = phi[2][i][j] = 1; // 第三个 
+            }
+          }
+        }
+        else if (n == 3 || n == 4) {
+          // 上下的两个晶粒初始化
+          if (j <= 3 || j >= Ny - 3) {
+            if (n == 3 && (i <= Nx / 3 + 3 && i >= Nx / 3 - 3)) {
+              phi[3][i][j] = phi_b[3][i][j] = 1; // 第四个 
+            }
+            else if (n == 4 && (i <= (Nx / 3) * 2 + 3 && i >= (Nx / 3) * 2 - 3)) {
+              phi[4][i][j] = phi_b[4][i][j] = 1; // 第五个 
+            }
+          }
+        }
+        // 四个角的晶粒
+        else if (n == 5){
+          if ((i <= 3 && j <= 3) || (i >= Nx - 3 && j <= 3) || (i >= Nx - 3 && j >= Ny - 3) || (i <= 3 && j >= Ny - 3)) {
+            phi[5][i][j] = phi_b[5][i][j] = 1; // 第六个 
+          }
+        }
+      }
+    }
+  }
 
-	
-	int timeInterval = 0.1; // ¼ÇÂ¼Ê±¼ä²½³¤
-	int allTime = 100; // ¾§Á£Éú³¤µÄ×ÜÊ±¼ä
-	double garma = 0.0208; // µ¥Î» J/m2
-	double thigma = 7 * 0.5 *  pow(10, -6); // thigmaÎª7 * delta x£¬ ¶ødelta xÎª0.5um£¬ÕâÀïÎÒÃÇ»»ËãÎªM×öµ¥Î» 
-	double W = 4 * garma / thigma; 
-	double a = (2 / PI) * pow(2 * thigma * garma, 0.5);
-	double deltaE = 0.09; // µ¥Î»Mpa
-	double Qb = 110; // µ¥Î» j/mol 
-	double R = 8.314 * pow(10, -3); // µ¥Î» kj/(K*mol) 
-	double T = 800; // ÕâÀïµÄTÎª¿ª¶ûÎÄÎÂ¶È 
-	double M = (0.139 / 800) * exp(-Qb/(R * T)); // M 0.139µÄµ¥Î»ÊÇm2£¬ÊÇM0µÄ±íÊ¾ 
-//	cout << garma << endl << thigma << endl << W << endl << a << endl << deltaE << endl << Qb << endl << R << endl << T << endl << M << endl;		 
-	
-	int time = 0; // 时间初始化
 
-	
 
-		
-	cout << "ending" << endl;
-	return 0;
+  // 测试初始化使用  
+  // 通过以下程序可以测试出在初始化状态下每个晶粒的phi不为0的位置。 
+  //for (int i = 0; i < nx; i++)
+  //{
+  //  for (int j = 0; j < ny; j++)
+  //  {
+  //    if (phi[2][i][j] == 1) {
+  //      cout << i << '\t' << j << endl;
+  //    }
+  //  }
+  //}
+
+  //  cout << phi[3][62][198] << phi[3][70][1] << phi[3][67][196] << phi[3][68][2] << endl; // 测试初始化使用 
+  //  cout << "上一个时刻第六个晶粒在原点处的phi值" << phi_b[5][0][0] << endl; // 测试phi_b的初始化使用 
+
+
+    //double timeInterval = 0.1; // 记录时间步长
+  double deltaT = 1; // 时间间
+  int allTime = 100; // 晶粒生长的总时间
+  double garma = 0.0208; // 单位 J/m2
+  double thigma = 7 * 0.5 *  pow(10, -6); // thigma为7 * delta x， 而delta x为0.5um，这里我们换算为M做单位 
+  double W = 4 * garma / thigma;
+  double a = (2 / PI) * pow(2 * thigma * garma, 0.5);
+  double deltaE = 0.09; // 单位Mpa
+  double Qb = 110; // 单位 j/mol 
+  double R = 8.314 * pow(10, -3); // 单位 kj/(K*mol) 
+  double T = 800; // 这里的T为开尔文温度 
+  double M = (0.139 / 800) * exp(-Qb / (R * T)); // M 0.139的单位是m2，是M0的表示 
+  //cout << garma << endl << thigma << endl << W << endl << a << endl << deltaE << endl << Qb << endl << R << endl << T << endl << M << endl;    
+
+  
+  //cout << n << endl << i << endl << j << endl;
+
+  int time = 0;
+  while (time < allTime) {
+    int k = 0; // 用于标记和n临界的晶粒
+    for (n = 0; n < 6; n++) {
+      for (i = 0; i < Nx; i++) {
+        for (j = 0; j < Ny; j++) {
+          double temp = 0;
+          double dif = 0;
+          for (k = 0; k < 6; k++) {
+            if (k == n) {
+              continue;
+            }
+            dif = (( phi[k][i + 1][j] + phi[k][i - 1][j] + phi[k][i][j + 1] + phi[k][i][j - 1] - 4 * phi[k][i][j]) - (phi[n][i + 1][j] + phi[n][i - 1][j] + phi[n][i][j + 1] + phi[n][i][j - 1] - 4 * phi[n][i][j])) / pow(deltaX, 2);
+            temp += (M / N)*(W * (phi[k][i][j] - phi[n][i][j]) + 0.5 * pow(a, 2) * (dif) - (8 / PI) * pow(phi[n][i][j] * phi[k][i][j], 0.5) * deltaE);
+          }
+          phi_b[n][i][j] = temp * deltaT + phi[n][i][j];
+        }
+      }
+    }
+    time += deltaT;
+  }
+
+
+  cout << "ending" << endl;
+  return 0;
 }
-
