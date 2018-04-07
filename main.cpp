683c601137fe3f561452ccd4e7684ffb3e888268
zzw.0628 <zhuzhenwei789@gmail.com>
2018-04-07 22:02:53 +0800

    s

--- main.cpp
+++ main.cpp
@@ -6,14 +6,14 @@ using namespace std;
 int main()
 { 
     cout << "begining" << endl;
-	// 6������������Ϊ200 * 200 
+	// 6¸ö¾§Á££¬Íø¸ñÎª200 * 200 
 	const int N = 6; 
 	const int Nx = 100;
 	const int Ny = 100;
-	double phi[N][Nx][Ny]; // ��ǰʱ�̵�phi
-	double phi_b[N][Nx][Ny]; // ��һʱ�̵�phi_b 
+	double phi[N][Nx][Ny]; // µ±Ç°Ê±¿ÌµÄphi
+	double phi_b[N][Nx][Ny]; // ÉÏÒ»Ê±¿ÌµÄphi_b 
 	
-	// ��ʼ��ÿ�������ڲ�ͬ���񴦵�phiֵ 
+	// ³õÊ¼»¯Ã¿¸ö¾§Á£ÔÚ²»Í¬Íø¸ñ´¦µÄphiÖµ 
 	int n, i, j;
 	for (n = 0; n < N; n++)
 	{
@@ -21,38 +21,38 @@ int main()
 		{
 			for (j = 0; j < Ny; j++)
 			{ 
-				phi[n][i][j] = phi_b[n][i][j] = 0; // �����������������phi������Ϊ0��
+				phi[n][i][j] = phi_b[n][i][j] = 0; // ½«¾§Á£ÔÚËùÓÐÍø¸ñµÄphi¶¼ÉèÖÃÎª0£»
 				
-				// ��6���������г�ʼ�������ض�λ�õ�phiΪ1������λ�ö���0
-				// �м���������г�ʼ�� 
+				// ½«6¸ö¾§Á£½øÐÐ³õÊ¼»¯£¬¼´ÌØ¶¨Î»ÖÃµÄphiÎª1£¬ÆäËûÎ»ÖÃ¶¼ÊÇ0
+				// ÖÐ¼äµÄÈý¸ö½øÐÐ³õÊ¼»¯ 
 				if (n == 0 || n == 1 || n == 2) {
 					if (j <= Ny/2 + 3 && j >= Ny/2 - 3){
 					    if (n == 0 && (i <= Nx/6 + 3 && i >= Nx/6 - 3)) {
-					    	phi[0][i][j] = phi_b[0][i][j] = 1; // ��һ�� 
+					    	phi[0][i][j] = phi_b[0][i][j] = 1; // µÚÒ»¸ö 
 					    }
 					    else if (n == 1 && (i <= (Nx/6)*3 + 3 && i >= (Nx/6)*3 - 3 )) {
-					    	phi[1][i][j] = phi_b[1][i][j] = 1; // �ڶ��� 
+					    	phi[1][i][j] = phi_b[1][i][j] = 1; // µÚ¶þ¸ö 
 					    }
 					    else if (n == 2 && (i <= (Nx/6)*5 + 3 && i >= (Nx/6)*5 - 3 )) {
-					    	phi[2][i][j] = phi[2][i][j] = 1; // ������ 
+					    	phi[2][i][j] = phi[2][i][j] = 1; // µÚÈý¸ö 
 				    	}
 					} 
 			    } 
 				else if (n == 3 || n == 4) {
-					// ���µ�����������ʼ��
+					// ÉÏÏÂµÄÁ½¸ö¾§Á£³õÊ¼»¯
 				    if (j <= 3 || j >= Ny - 3) {
 						if (n == 3 && (i <= Nx/3 + 3 && i >= Nx/3 - 3)) {
-							phi[3][i][j] = phi_b[3][i][j] = 1; // ���ĸ� 
+							phi[3][i][j] = phi_b[3][i][j] = 1; // µÚËÄ¸ö 
 						}
 						else if (n == 4 && (i <= (Nx/3)*2 + 3 && i >= (Nx/3)*2 - 3)) {
-							phi[4][i][j] = phi_b[4][i][j] = 1; // ����� 
+							phi[4][i][j] = phi_b[4][i][j] = 1; // µÚÎå¸ö 
 						}
 					} 
 				}
-				// �ĸ��ǵľ���
+				// ËÄ¸ö½ÇµÄ¾§Á£
 				else if (n == 5){
 					if ((i <= 3 && j <= 3) || (i >= Nx - 3 && j <= 3) || (i >= Nx - 3 && j >= Ny - 3) || (i <= 3 && j >= Ny - 3)) {
-						phi[5][i][j] = phi_b[5][i][j] = 1; // ������ 
+						phi[5][i][j] = phi_b[5][i][j] = 1; // µÚÁù¸ö 
 					}
 				}
 			}
@@ -61,8 +61,8 @@ int main()
 	
 	
 
-// ���Գ�ʼ��ʹ��  
-// ͨ�����³�����Բ��Գ��ڳ�ʼ��״̬��ÿ��������phi��Ϊ0��λ�á� 
+// ²âÊÔ³õÊ¼»¯Ê¹ÓÃ  
+// Í¨¹ýÒÔÏÂ³ÌÐò¿ÉÒÔ²âÊÔ³öÔÚ³õÊ¼»¯×´Ì¬ÏÂÃ¿¸ö¾§Á£µÄphi²»Îª0µÄÎ»ÖÃ¡£ 
 		for (int i = 0; i < Nx; i++)
 		{
 			for (int j = 0; j < Ny; j++)
@@ -73,23 +73,25 @@ int main()
 			}
 		}
 
-//  cout << phi[3][62][198] << phi[3][70][1] << phi[3][67][196] << phi[3][68][2] << endl; // ���Գ�ʼ��ʹ�� 
-//	cout << "��һ��ʱ�̵�����������ԭ�㴦��phiֵ" << phi_b[5][0][0] << endl; // ����phi_b�ĳ�ʼ��ʹ�� 
+//  cout << phi[3][62][198] << phi[3][70][1] << phi[3][67][196] << phi[3][68][2] << endl; // ²âÊÔ³õÊ¼»¯Ê¹ÓÃ 
+//	cout << "ÉÏÒ»¸öÊ±¿ÌµÚÁù¸ö¾§Á£ÔÚÔ­µã´¦µÄphiÖµ" << phi_b[5][0][0] << endl; // ²âÊÔphi_bµÄ³õÊ¼»¯Ê¹ÓÃ 
 
 	
-	int timeInterval = 0.1; // ��¼ʱ�䲽��
-	int allTime = 100; // ������������ʱ��
-	double garma = 0.0208; // ��λ J/m2
-	double thigma = 7 * 0.5 *  pow(10, -6); // thigmaΪ7 * delta x�� ��delta xΪ0.5um���������ǻ���ΪM����λ 
+	int timeInterval = 0.1; // ¼ÇÂ¼Ê±¼ä²½³¤
+	int allTime = 100; // ¾§Á£Éú³¤µÄ×ÜÊ±¼ä
+	double garma = 0.0208; // µ¥Î» J/m2
+	double thigma = 7 * 0.5 *  pow(10, -6); // thigmaÎª7 * delta x£¬ ¶ødelta xÎª0.5um£¬ÕâÀïÎÒÃÇ»»ËãÎªM×öµ¥Î» 
 	double W = 4 * garma / thigma; 
 	double a = (2 / PI) * pow(2 * thigma * garma, 0.5);
-	double deltaE = 0.09; // ��λMpa
-	double Qb = 110; // ��λ j/mol 
-	double R = 8.314 * pow(10, -3); // ��λ kj/(K*mol) 
-	double T = 800; // �����TΪ�������¶� 
-	double M = (0.139 / 800) * exp(-Qb/(R * T)); // M 0.139�ĵ�λ��m2����M0�ı�ʾ 
+	double deltaE = 0.09; // µ¥Î»Mpa
+	double Qb = 110; // µ¥Î» j/mol 
+	double R = 8.314 * pow(10, -3); // µ¥Î» kj/(K*mol) 
+	double T = 800; // ÕâÀïµÄTÎª¿ª¶ûÎÄÎÂ¶È 
+	double M = (0.139 / 800) * exp(-Qb/(R * T)); // M 0.139µÄµ¥Î»ÊÇm2£¬ÊÇM0µÄ±íÊ¾ 
 //	cout << garma << endl << thigma << endl << W << endl << a << endl << deltaE << endl << Qb << endl << R << endl << T << endl << M << endl;		 
 	
+	int time = 0; // 时间初始化
+
 	
 
 		
